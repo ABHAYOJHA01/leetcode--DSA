9569fe2256b27672c4e4ace64039b9d114383c7f
Abhay ojha <ABHAYOJHA963@GMAIL.COM>
2025-11-18 22:13:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (5.33%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
@@ -6,9 +6,9 @@ class Solution {
         
         while (i < n - 1) {
             if (bits[i] == 1) {
-                i += 2; 
+                i = i+2; 
             } else {
-                i += 1; 
+                i = i+1; 
             }
         }
 
