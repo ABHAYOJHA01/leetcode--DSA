a39dbc6fd623c9bf4b34ecd0c75ca4c01318fcec
Abhay ojha <ABHAYOJHA963@GMAIL.COM>
2025-11-23 11:08:40 +0530

    Time: 16 ms (28.57%) | Memory: 61 MB (6.21%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,19 +1,21 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
-
+        
         Integer[][] memo = new Integer[nums.length][3];
+
         return helper(nums,0,0,memo);
     }
 
-   static int helper(int[] nums,int i,int sumMod, Integer[][] memo){
+    static int helper(int[] nums,int i,int sum,Integer[][] memo){
+          
+          if (i == nums.length) return sum == 0 ? 0 : Integer.MIN_VALUE;
+          
+          if(memo[i][sum] != null) return memo[i][sum];
 
-        if (i == nums.length) return sumMod == 0 ? 0 : Integer.MIN_VALUE;
+          int left =nums[i]+ helper(nums,i+1,(sum+nums[i])%3,memo);
+          int right =helper(nums,i+1,sum,memo);
 
-        if (memo[i][sumMod] != null) return memo[i][sumMod];
+           return memo[i][sum] = Math.max(left, right);
 
-          int take = nums[i] + helper(nums, i+1, (sumMod + nums[i]) % 3, memo);
-          int skip = helper(nums, i+1, sumMod, memo);
-
-         return memo[i][sumMod] = Math.max(take, skip);
     }
 }
