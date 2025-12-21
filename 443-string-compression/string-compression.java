cb72475ac882b50b61b175b19db06f081a946c86
Abhay ojha <ABHAYOJHA963@GMAIL.COM>
2025-12-21 17:10:09 +0530

    Time: 1 ms (99.73%) | Memory: 45.5 MB (34.14%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0; 
+        int count = 1;
+
+        for (int i = 1; i <= chars.length; i++) {
+            if (i < chars.length && chars[i] == chars[i - 1]) {
+                count++;
+            } else {
+                chars[index++] = chars[i - 1];
+
+                if (count > 1) {
+                    String num = String.valueOf(count);
+                    for (char c : num.toCharArray()) {
+                        chars[index++] = c;
+                    }
+                }
+                count = 1;
+            }
+        }
+        return index;
+    }
+}
