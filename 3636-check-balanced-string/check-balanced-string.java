caceebab61b9763841d6cae1a9c875bdfe0a302e
Abhay ojha <ABHAYOJHA963@GMAIL.COM>
2026-02-23 23:06:16 +0530

    Time: 1 ms (98.44%) | Memory: 43.6 MB (66.19%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int evensum=0;
+        int oddsum=0;
+        for(int i=0;i<num.length();i++){
+            int digit=num.charAt(i)-'0';
+            if(i%2==0){
+                evensum = evensum+digit;
+
+            }else{
+                oddsum=oddsum+digit;
+            }
+        }
+        return evensum==oddsum;
+    }
+}
